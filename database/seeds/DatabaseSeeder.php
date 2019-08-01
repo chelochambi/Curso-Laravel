8816de5752026e840c612b5fd44007eab006fb1c
sofi_laravel <marcelochambiparedes@gmail.com>
2019-07-31 22:58:57 -0400

    Trabajando con seeders y factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -9,8 +10,21 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
+    public function run(){
+        $this->truncateTablas([
+            'rol',
+            'permiso'
+        ]);
         // $this->call(UsersTableSeeder::class);
+        $this->call(tablaRolSeeder::class);
+        $this->call(tablaPermisoSeeder::class);
+    }
+
+    protected function truncateTablas(array $tablas){
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        foreach ($tablas as $tabla){
+            DB::table($tabla)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
